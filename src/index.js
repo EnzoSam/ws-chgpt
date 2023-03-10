2ac0c15032661f3699a76c09c0123bff608e10de
Enzo Samudio <enzosamudio@gmail.com>
2023-03-10 20:01:35 -0300

    correccion en puerto

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ console.log("hola mundo")
 var app = require('./app');
 var port = process.env.PORT || 3999;
 
-app.listen(process.env.PORT, function () {
-    console.log("Escuchando en puerto " + listener.address().port);
+app.listen(port, function () {
+    console.log("Escuchando en puerto " + port);
   });
