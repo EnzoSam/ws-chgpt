0363aefd830383d70b1d26c194be772f73bf67c6
enzo256 <enzosamudio@gmail.com>
2023-03-11 21:25:55 -0300

    correccion en chat

--- src/controller/wsController.controller.js
+++ src/controller/wsController.controller.js
@@ -26,8 +26,7 @@ var controller = {
         ChatGPTController.chat(textMessage).
         then(response=>
           {
-            console.log(response);
-            res.status(200).send(response.data.choices[0].text);
+            WsController.sendTextMessage(response.data.choices[0].text, "5493751446485");
           })
           .catch(error =>{
             console.log(error);
