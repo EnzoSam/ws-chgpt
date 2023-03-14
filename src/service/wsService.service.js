29a21fc0200f6666c0006608e1fbecf519be850a
Enzo Samudio <enzosamudio@gmail.com>
2023-03-14 18:43:13 -0300

    correccio en ws

--- src/service/wsService.service.js
+++ src/service/wsService.service.js
@@ -1,63 +1,56 @@
 const { sendMessage, getTextMessageInput } = require("./whatsappHelper");
 
-exports.test = async function() {
-    console.log("WSService Ok");
+exports.test = async function () {
+  console.log("WSService Ok");
 };
 
-exports.sendTextMessage = function(messageText, phoneNumber) {
+exports.sendTextMessage = function (messageText, phoneNumber) {
+  let promise = new Promise((resolve, reject) => {
+    try {
+      console.log(messageText);
+      if (messageText === null || messageText === "") {
+        resolve();
+      } else {
+        var data = getTextMessageInput(phoneNumber, messageText);
+        sendMessage(data)
+          .then(function (response) {
+            resolve();
+          })
+          .catch(function (error) {
+            console.log(error);
+            reject(error);
+          });
+      }
+    } catch (ex) {
+      reject(ex);
+    }
+  });
 
-    console.log(messageText);
-    if(messageText === null || messageText === '')
-    return;
-    var data = getTextMessageInput(phoneNumber, messageText);
-    sendMessage(data)
-      .then(function (response) {
-
-        console.log('enviadoooooo');
-            console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-        console.log(error.response.data);
-        res.sendStatus(500);
-        return;
-      });
-  
+  return promise;
 };
 
-exports.getMessageTextFromWhebhookObject = function(whebhookObject) {
+exports.getMessageTextFromWhebhookObject = function (whebhookObject) {
+  if (!whebhookObject.entry) return null;
 
-    if(!whebhookObject.entry)
-        return null;
-    
-    if(!whebhookObject.entry[0].changes)
-        return null;
+  if (!whebhookObject.entry[0].changes) return null;
 
-    if(!whebhookObject.entry[0].changes[0].value)
-        return null;
+  if (!whebhookObject.entry[0].changes[0].value) return null;
 
-    if(!whebhookObject.entry[0].changes[0].value.messages)
-        return null;        
+  if (!whebhookObject.entry[0].changes[0].value.messages) return null;
 
-    if(!whebhookObject.entry[0].changes[0].value.messages[0].text)
-        return null;           
+  if (!whebhookObject.entry[0].changes[0].value.messages[0].text) return null;
 
-    return  whebhookObject.entry[0].changes[0].value.messages[0].text.body;
+  return whebhookObject.entry[0].changes[0].value.messages[0].text.body;
 };
 
-exports.getFromNumberTextFromWhebhookObject = function(whebhookObject) {
+exports.getFromNumberTextFromWhebhookObject = function (whebhookObject) {
+  if (!whebhookObject.entry) return null;
 
-    if(!whebhookObject.entry)
-        return null;
-    
-    if(!whebhookObject.entry[0].changes)
-        return null;
+  if (!whebhookObject.entry[0].changes) return null;
 
-    if(!whebhookObject.entry[0].changes[0].value)
-        return null;
+  if (!whebhookObject.entry[0].changes[0].value) return null;
 
-    if(!whebhookObject.entry[0].changes[0].value.messages)
-        return null;        
+  if (!whebhookObject.entry[0].changes[0].value.messages) return null;
 
-    return whebhookObject.entry[0].changes[0].value.messages[0].from
+  return whebhookObject.entry[0].changes[0].value.messages[0].from;
 };
