9d54777dbfdd24515189f6e3c5673bba12da4f2b
Enzo Samudio <enzosamudio@gmail.com>
2023-03-27 19:40:13 -0300

    creacion de ruta detail tiket

--- src/route/tikets.route.js
+++ src/route/tikets.route.js
@@ -5,7 +5,7 @@ var TiketsController = require('../controller/tikets.controller');
 var router = express.Router();
 
 router.get("/:state", TiketsController.getTikets);
-router.get("detail/:id", TiketsController.getTiket);
+router.get("/detail/:id", TiketsController.getTiket);
 router.post("/", TiketsController.update);  
 
 module.exports = router;
