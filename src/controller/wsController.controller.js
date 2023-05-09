6d07cfa4e5a98acac4f079671f53b6a6cf1af5cb
Enzo Samudio <enzosamudio@gmail.com>
2023-05-09 11:28:56 -0300

    conexion de embeddings con whatsapp

--- src/controller/wsController.controller.js
+++ src/controller/wsController.controller.js
@@ -1,6 +1,7 @@
-const WsService = require('../service/wsService.service');
-const ChatGPTController = require('../service/chatgpt.service');
-const TiketService = require('../service/tiket.service');
+const WsService = require("../service/wsService.service");
+const ChatGPTController = require("../service/chatgpt.service");
+const TiketService = require("../service/tiket.service");
+const EmbeddingService = require('../service/embeddings.service');
 
 var controller = {
   test: function (req, res) {
@@ -18,73 +19,110 @@ var controller = {
       res.sendStatus(400);
     }
   },
-  processMessage:function (request, res) {
-      try{
-        
-        let textMessage = WsService.getMessageTextFromWhebhookObject(request.body);
-        if(textMessage === null  || textMessage === '')
-        {
-            console.log('no se pudo procesar el mensaje');
-            res.sendStatus(200);
-        }
-        else
-        {
-          wID = WsService.getFromNumberTextFromWhebhookObject(request.body);
-          wName = WsService.getProfileNameFromWhebhookObject(request.body);
-          console.log('wID ' + wID);
-          console.log('wName ' + wName);
-          if(wID && wID != null && wID != '')
-            TiketService.verifyTiket(wID,wName,textMessage);
-          
-          res.sendStatus(200);
-        }             
-    }
-    catch(ex)
-    {
-        console.log(ex);
+  processMessage: function (request, res) {
+    try {
+      let textMessage = WsService.getMessageTextFromWhebhookObject(
+        request.body
+      );
+      if (textMessage === null || textMessage === "") {
+        console.log("no se pudo procesar el mensaje");
         res.sendStatus(200);
-    } 
+      } else {
+        wID = WsService.getFromNumberTextFromWhebhookObject(request.body);
+        wName = WsService.getProfileNameFromWhebhookObject(request.body);
+        console.log("wID " + wID);
+        console.log("wName " + wName);
+        if (wID && wID != null && wID != "")
+          TiketService.verifyTiket(wID, wName, textMessage);
+
+        res.sendStatus(200);
+      }
+    } catch (ex) {
+      console.log(ex);
+      res.sendStatus(200);
+    }
   },
-  notify:function (request, res) {
-    try{
-      
-        let textMessage = WsService.getMessageTextFromWhebhookObject(request.body);
-        if(textMessage === null  || textMessage === '')
-        {
-            //console.log(request.body);
-        }
-        else
-        {
-          wID = WsService.getFromNumberTextFromWhebhookObject(request.body);
-          wName = WsService.getProfileNameFromWhebhookObject(request.body);
-          TiketService.verifyTiket(wID,wName,textMessage);
-          ChatGPTController.chat(textMessage).
-          then(response=>
-            {
-              let dest = WsService.getFromNumberTextFromWhebhookObject(request.body);
-              WsService.sendTextMessage(response, dest).then(()=>
-                {
-                  console.log('enviado');
-                  res.sendStatus(200);
-                }).catch(error=>
-                {
-                  console.log(error);
-                  res.sendStatus(200);
-                });
-            })
-            .catch(error =>{
-              console.log(error);
-              res.sendStatus(200);
-          }); 
-        }     
-        
-    }
-    catch(ex)
-    {
-        console.log(ex);
+  processMessagePrana: function (request, res) {
+    try {
+      let textMessage = WsService.getMessageTextFromWhebhookObject(
+        request.body
+      );
+      if (textMessage === null || textMessage === "") {
+        console.log("no se pudo procesar el mensaje");
         res.sendStatus(200);
-    }    
-  }
+      } else {
+        wID = WsService.getFromNumberTextFromWhebhookObject(request.body);
+        wName = WsService.getProfileNameFromWhebhookObject(request.body);
+        console.log("wID " + wID);
+        console.log("wName " + wName);
+        if (wID && wID != null && wID != "")
+        {
+          TiketService.verifyTiket(wID, wName, textMessage).then(()=>
+            {
+              EmbeddingService.getMostSimilarParagraph(textMessage).then
+              (()=>
+                {
+                  let t = 'ni idea';
+                  if(p && p != undefined && p != null)
+                  {
+                    t = p.text;
+                  }
+
+                  WsService.sendTextMessage(t, wID)
+                  .then(() => {
+                    console.log("enviado");
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });                  
+                })
+
+            });
+          }
+
+        res.sendStatus(200);
+      }
+    } catch (ex) {
+      console.log(ex);
+      res.sendStatus(200);
+    }
+  },
+  notify: function (request, res) {
+    try {
+      let textMessage = WsService.getMessageTextFromWhebhookObject(
+        request.body
+      );
+      if (textMessage === null || textMessage === "") {
+        //console.log(request.body);
+      } else {
+        wID = WsService.getFromNumberTextFromWhebhookObject(request.body);
+        wName = WsService.getProfileNameFromWhebhookObject(request.body);
+        TiketService.verifyTiket(wID, wName, textMessage);
+        ChatGPTController.chat(textMessage)
+          .then((response) => {
+            let dest = WsService.getFromNumberTextFromWhebhookObject(
+              request.body
+            );
+            WsService.sendTextMessage(response, dest)
+              .then(() => {
+                console.log("enviado");
+                res.sendStatus(200);
+              })
+              .catch((error) => {
+                console.log(error);
+                res.sendStatus(200);
+              });
+          })
+          .catch((error) => {
+            console.log(error);
+            res.sendStatus(200);
+          });
+      }
+    } catch (ex) {
+      console.log(ex);
+      res.sendStatus(200);
+    }
+  },
 };
 
 module.exports = controller;
